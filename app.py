bf0e396d27f1c75ec7c00390a7d67d88bdde7c4f
Samir Khan <sksamirkhan83@gmail.com>
2026-08-01 10:32:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,6 +142,18 @@ if (user_input) & (leader_agent):
                 except Exception as err:
                     st.error("Error Code:", err)
 
+    with tab3:
+        if st.button("Click To Generate PPT", key = "PPT-Button"):
+            with st.spinner("Running Agent"):
+                try:
+                    code = run_agent(leader_agent,user_input)
+                    st.html(code, width = "stretch", unsafe_allow_javascript = True)
+
+                    if st.download_button(label = "DOWNLOAD PPT",data = code,file_name = "ppt.html",mime = "text/html"):
+                        st.success("PPT Downloaded Successfully!!")
+                except Exception as err:
+                    st.error("Error Code: ", err)
+
                     
                     
         
